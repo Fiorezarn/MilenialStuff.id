4b4c73c8dfd4d53d1b5d4be380adcff73451ce7c
Nama Anda <fioreza1902@gmail.com>
2023-06-05 11:40:58 +0700

    fix: banner product

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,7 +1,7 @@
 @section('title', 'Product')
 <div>
     <div class="banner-product">
-        <img class="gambar-banner" src="img/product-content.jpeg" alt="banner-produk" href="#">
+        <img class="gambar-banner" src="img/product-content.webp" alt="banner-produk" href="#">
     </div>
     <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
@@ -46,10 +46,6 @@
             </div>
         </div>
     @endforeach
-
-
-
-
 
 
 
