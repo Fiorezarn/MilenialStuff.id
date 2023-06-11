99a1ebeb7151910811326c551217ba828510aa30
Nama Anda <fioreza1902@gmail.com>
2023-06-11 22:45:01 +0700

    fix: icon browser

--- resources/views/layouts/product-layouts.blade.php
+++ resources/views/layouts/product-layouts.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Icon Browser -->
-    <link rel="shortcut icon" type="x-icon" sizes="180x180" href="img/iconbrowser.ico">
+    <link rel="icon" type="image/x-icon" href={{ asset('img/android-chrome-large.png') }}>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
